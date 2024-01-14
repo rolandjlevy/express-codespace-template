d3c3b76e4d6707d21f4bcfb57cb2c5ab7cc2c656
Roland Levy <contact@rolandlevy.co.uk>
2024-01-14 20:49:00 +0000

    Fixed linting issues

--- index.js
+++ index.js
@@ -6,5 +6,5 @@ const port = process.env.PORT || 3000;
 app.get('/', (req, res) => res.send('Hello World!'));
 
 app.listen(port, () => {
-  console.log(`Listening on port:${port}`)
+  console.log(`Listening on port:${port}`);
 });
