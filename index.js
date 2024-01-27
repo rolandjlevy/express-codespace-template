bbe8119b14bce16ef0e8e34a9e935a76904896e3
Roland Levy <contact@rolandlevy.co.uk>
2024-01-27 20:41:27 +0000

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
 app.get('/', (req, res) => res.send('Hello World!'));
 
 app.listen(port, () => {
